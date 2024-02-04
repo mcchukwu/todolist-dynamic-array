6ace353b4a9a84205008596fa02b353b2d43c964
Miracle Chukwu <miraclechukwu2011@gmail.com>
2024-02-04 13:06:41 +0100

    Initial commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+const todos = [
+    {
+        Header: "Wash the plates",
+        description: "I have to wash the plates after the party",
+    },
+    {
+        Header: "Clean the room",
+        description:
+            "Do the detailing, sweep and mop all areaas with touch of fineese and hertfelt gratitude to be alife on the earth",
+    },
+];
+
+//
